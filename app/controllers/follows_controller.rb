1f4c0d38e83c861ae8b96a510150daa2b0289428
Ben Call <bgcall@indiana.edu>
2017-03-05 17:13:11 -0500

    follow fixed

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,14 +2,17 @@ class FollowsController < ApplicationController
 
   before_action :require_user
 
-  def create
+  def follow_unfallow_toggle
     followee = User.find(params[:id])
-    current_user.follow!(followee)
+    current_user.toggle_follow!(followee)
+    result = current_user.follows?(followee) ?
+      "followed" : "unfollowed"
+    render json: {result: result}
   end
 
-  def destroy
-    followee = User.find(params[:id])
-    current_user.unfollow!(followee)
-  end
+  # def destroy
+  #   followee = User.find(params[:id])
+  #   current_user.unfollow!(followee)
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 end
 
 post '/login' => 'sessions#create'
+post '/users/:id/follow' => 'follows#follow_unfallow_toggle'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
