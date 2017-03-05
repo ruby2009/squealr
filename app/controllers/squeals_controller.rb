992d1308df5df2575d5a9f3a50091e275262b81c
Ben Call <bgcall@indiana.edu>
2017-03-05 12:26:11 -0500

    squeals change 2

--- app/controllers/squeals_controller.rb
+++ app/controllers/squeals_controller.rb
@@ -2,7 +2,7 @@ class SquealsController < ApplicationController
 
   before_action :require_user
 
-  def index(current_user)
+  def index
     @squeals = Squeal.timeline(current_user)
     render json: @squeals, scope: current_user,
     scope_name: :current_user
