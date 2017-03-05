71312ee4653fcf029ba6aac5d1781f6fbe8becce
Ben Call <bgcall@indiana.edu>
2017-03-05 14:22:37 -0500

    user attributess added

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :avatar 
-  # :following
-  #
-  # def following
-  #     current_user.follows?(object) if current_user
-  # end
+  attributes :username, :avatar, :following
+
+  def following
+      current_user.follows?(object) if current_user
+  end
 end
