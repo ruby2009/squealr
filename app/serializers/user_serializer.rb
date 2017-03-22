117211669fe109d3fca7f618bf248cc3bc670a94
Ben Call <bgcall@indiana.edu>
2017-03-22 17:48:56 -0400

    set avatar to default url in serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,4 +4,8 @@ class UserSerializer < ActiveModel::Serializer
   def following
       current_user.follows?(object) if current_user
   end
+
+  def avatar
+    "https://cdn3.iconfinder.com/data/icons/rcons-user-action/32/girl-512.png"
+  end
 end
